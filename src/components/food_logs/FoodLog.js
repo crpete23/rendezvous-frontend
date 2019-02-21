b490f90ba4a08d1988167edbd895642e4d2af7a9
Christopher Peterson <crpete23@gmail.com>
2019-02-21 11:47:39 -0800

    adjusted food log to show meals and ingredients

--- src/components/food_logs/FoodLog.js
+++ src/components/food_logs/FoodLog.js
@@ -5,29 +5,36 @@ import { bindActionCreators } from 'redux'
 
 import { deleteFoodLog } from '../../actions/food_log.actions'
 
-const FoodLog = ({id, name, calories, protein, carbs, fat, PUFAs, user_id, date, time, deleteLog}) => {
-  const dateString = `${date.toString().slice(0,2)}/${date.toString().slice(2,4)}/${date.toString().slice(4)}`
+const FoodLog = ({id, time, food, deleteLog}) => {
+
+  const ingredientsList = food.ingredients.length ? food.ingredients.map((ingredient)=>{
+    return ingredient.name
+  }).join(', ') : null;
+
+  const mealsList = food.meals.length ? food.meals.map((meal)=>{
+    return `${meal.name} (${meal.ingredients.list.length ? meal.ingredients.list.map((ingredient)=>{ return `${ingredient.quantity} ${ingredient.name}`}).join(', ') : null })`
+  }).join(', ') : null;
 
   function upperCase (string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function timeToString(time){
-  var min = time%100;
-  var hour = (time%10000-min)/100;
-  var end = hour>11? 'PM' : 'AM';
-
-  if(min<10){
-    min='0'+min;
   }
 
-  if(hour>12){
-    hour=hour-12;
-  } else if (hour===0){
-    hour=12;
+  function timeToString(time){
+    var min = time%100;
+    var hour = (time%10000-min)/100;
+    var end = hour>11? 'PM' : 'AM';
+
+    if(min<10){
+      min='0'+min;
+    }
+
+    if(hour>12){
+      hour=hour-12;
+    } else if (hour===0){
+      hour=12;
+    }
+    return `${hour}:${min} ${end}`
   }
-  return `${hour}:${min} ${end}`
-}
 
   return (
     <List.Item>
@@ -37,7 +44,10 @@ function timeToString(time){
      <List.Content>
       <List.Header>{timeToString(time)}</List.Header>
       <List.Description>
-          {`Meal: ${name}, calories: ${calories}, protein: ${protein}, carbs: ${carbs}, fat: ${fat}, PUFAs: ${PUFAs}`}
+          {`Meals: ${mealsList}`}
+      </List.Description>
+      <List.Description>
+          {`Add Ons: ${ingredientsList}`}
       </List.Description>
      </List.Content>
    </List.Item>
